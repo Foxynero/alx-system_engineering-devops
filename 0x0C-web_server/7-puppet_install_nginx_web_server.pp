60b1f30fe0921c08f96f8aef5bb18fda57edbc14
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-05-04 15:21:14 +0000

    Web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,19 +2,20 @@
 
 package { 'nginx':
   ensure => installed,
-  name   => 'nginx'
 }
-file { 'path_to_html':
-  content => 'Hello World',
-  path    => '/var/www/html/index.nginx-debian.html'
+
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
-file_line { 'put_in_line':
-  ensure => present,
-  path   => 'etc/nginx/sites-available/default',
-  after  => '/listen 80 default_server;'
-  line   => 'rewrite ^/redirect_me HTTP/1.1 301 Moved Permanently;'
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
 }
+
 service { 'nginx':
   ensure  => running,
-  require => Package['nginx']
+  require => Package['nginx'],
 }
