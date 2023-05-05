b3c40b7825d7ebc50676fc77dde121dd7a4fcc9d
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-05-05 15:18:22 +0000

    Load balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,6 +4,11 @@ package { 'nginx':
   ensure => installed,
 }
 
+class { 'nginx::config::resource':
+  content => "add_header X-Served-By ${::hostname};",
+  target  => '/etc/nginx/conf.d/custom_header.conf',
+}
+
 file { '/etc/nginx/nginx.conf':
   ensure  => file,
   content => 'add_header X-Served-By $hostname;',
